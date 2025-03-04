cd435135dbb072c149372dae7f6deb3697c2cc3f
iamab00zar <rajabi.aboozar@gmail.com>
2025-03-04 14:10:47 +0100

    showing recipe info in show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,20 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show]
+
   def show
   end
 
   def search
-
     @recipes = Recipes::RecipeFinderService.new(params[:query]).call
 
     render "recipes/results"
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
